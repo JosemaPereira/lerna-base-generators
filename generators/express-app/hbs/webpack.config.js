df8fc26c2d867ad5679682913334729cf3191c3f
JosemaPereira <josema.pereira.cih@gmail.com>
2020-09-21 11:03:13 -0500

    fix(express-app): fix build and test scrips

--- generators/express-app/hbs/webpack.config.js
+++ generators/express-app/hbs/webpack.config.js
@@ -1,22 +1,28 @@
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    entry: './app',
-    devtool: 'source-map',
-    target: 'node',
-    externals: [nodeExternals()],
-    optimization: {
-        minimize: true
-    },
-    performance: {
-        hints: false
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            include: __dirname,
-            exclude: [/node_modules/, /test/]
-        }]
-    }
+  entry: './app',
+  devtool: 'source-map',
+  target: 'node',
+  externals: [
+    nodeExternals(),
+    { express: 'commonjs express' },
+    { require_optional: 'commonjs' },
+  ],
+  optimization: {
+    minimize: true,
+  },
+  performance: {
+    hints: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: __dirname,
+        exclude: [/node_modules/, /test/],
+      },
+    ],
+  },
 };
